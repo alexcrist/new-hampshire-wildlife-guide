4f25c5a378f2d9a20e54e7435f1aee45bcca7c15
Alex Crist <alexecrist@gmail.com>
2017-06-21 16:38:44 -0400

    Add web scraper

---
+++ scraper.py
@@ -0,0 +1,97 @@
+import json
+import os
+import re
+import requests
+from bs4 import BeautifulSoup
+from requests.utils import quote
+
+from utils import make_dir, download_image, clean_scientific_name, clean_common_names
+
+categories = [
+    { 'name': 'Alpine Flora', 'url': 'alpineflora' },
+    { 'name': 'Amphibians', 'url': 'amphibians' },
+    { 'name': 'Birds', 'url': 'birds' },
+    { 'name': 'Butterflies and Moths', 'url': 'butterflies-moths' },
+    { 'name': 'Ferns', 'url': 'ferns' },
+    { 'name': 'Fungi', 'url': 'fungi' },
+    { 'name': 'Insects', 'url': 'insects' },
+    { 'name': 'Lichens', 'url': 'lichens' },
+    { 'name': 'Mammals', 'url': 'mammals' },
+    { 'name': 'Reptiles', 'url': 'reptiles' },
+    { 'name': 'Trees', 'url': 'trees' },
+    { 'name': 'Wildflowers', 'url': 'wildflowers' }
+]
+
+url_base = 'http://amc-nh.org'
+url_head = url_base + '/resources/guides/'
+url_tail = '/index.php'
+output_dir = 'data'
+
+def main():
+    make_dir(output_dir)
+    data = {}
+
+    print('Scraping data from ' + url_head + '...')
+
+    for category in categories:
+        print('Starting ' + category['name'] + ' section...')
+        category_dir = output_dir + '/' + category['url']
+        make_dir(category_dir)
+        data[category['name']] = []
+
+        url = url_head + category['url'] + url_tail
+        page = requests.get(url)
+
+        soup = BeautifulSoup(page.text, 'lxml')
+        container = soup.find('ul', class_='clGalleryList')
+        items = container.find_all('li')
+
+        for item in items:
+            entry = {}
+            text = item.find('div')
+
+            # Extract scientific name
+            scientific_name = text.find('em')
+            for br in scientific_name.find_all('br'):
+                br.replace_with(' ')
+            entry['scientific_name'] = clean_scientific_name(scientific_name.text)
+            scientific_name.decompose()
+
+            # Skip if duplicate
+            is_duplicate = False
+            for existing_entry in data[category['name']]:
+                if existing_entry['scientific_name'] == entry['scientific_name']:
+                    is_duplicate = True
+                    break
+            if is_duplicate:
+                continue
+
+            # Extract common names
+            names = text.find('a')
+            *common_names, photographer = re.split(',|©\ ', names.text)
+            entry['common_names'] = clean_common_names(common_names)
+            entry['photographer'] = photographer
+
+            # Extract and download image
+            image = url_base + quote(item.find('a')['href'])
+            backup_image = url_base + quote(item.find('img')['src'])
+            extension = image[image.rfind('.'):]
+            image_path = category['url'] + '/' + re.sub(r'[^\w]', '', entry['scientific_name'])
+            image_path_full = output_dir + '/' + image_path + extension
+            success = True#download_image(image, image_path_full, backup_image)
+            if success:
+                entry['image'] = image_path
+
+            data[category['name']].append(entry)
+
+        print('Finished ' + category['name'] + ' section!')
+
+    # Store information
+    with open(output_dir + '/data.json', 'w') as file:
+        json.dump(data, file)
+
+    print('Scraping complete!')
+
+if __name__ == '__main__':
+    main()
+
---
+++ utils.py
@@ -0,0 +1,41 @@
+import os
+from urllib.request import urlretrieve
+
+def make_dir(dir_name):
+    if not os.path.exists(dir_name):
+        os.makedirs(dir_name)
+
+def download_image(image, image_path, backup_image=False):
+    try:
+        urlretrieve(image, image_path)
+        return True
+    except Exception as e:
+        print('Error downloading image from: ' + image)
+        if backup_image:
+            print('Downloading from backup source: ' + backup_image)
+            return download_image(backup_image, image_path)
+        else:
+            print('Download failed. No backup source.')
+            return False
+
+def clean_name(name):
+    open_paren = name.find('(')
+    close_paren = name.find(')') + 1
+    if open_paren != -1 and close_paren != -1:
+        name = name[:open_paren] + name[close_paren:]
+    name = name.lstrip().rstrip()
+    return name
+
+def clean_scientific_name(name):
+    comma = name.find(',')
+    if comma != -1:
+        name = name[:comma]
+    return clean_name(name)
+
+def clean_common_names(names):
+    clean_names = []
+    for name in names:
+        name = clean_name(name)
+        if name != '':
+            clean_names.append(name)
+    return clean_names
